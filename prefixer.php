33fd4e09e62150b297ed34704a24788990262226
Javier Marin <javiermarinros@gmail.com>
2012-02-14 12:04:50 +0000

    First commit

---
+++ prefixer.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * php_prefixer - Add vendor prefixes in your CSS files for cross browser compatibility
+ * 
+ * --
+ * Copyright (c) Javier Marín
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * --
+ * 
+ * @package		php_prefixer
+ * @link		https://github.com/javiermarinros/php_prefixer
+ * @version		1
+ * @author		Javier Marín <https://github.com/javiermarinros>
+ * @copyright           Javier Marín <https://github.com/javiermarinros>
+ * @license		http://opensource.org/licenses/mit-license.php MIT License
+ */
